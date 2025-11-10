1728f90465cc9f7545f3824f28a28b728d53c489
sameer-18sp <pandeyasameer4@gmail.com>
2025-11-10 11:33:51 +0530

    fix: Update build configuration and React imports

--- frontend/plant-disease/src/main.jsx
+++ frontend/plant-disease/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react'
 import App from './App.jsx'
-import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css'
 
 // // Access env variables with import.meta.env
